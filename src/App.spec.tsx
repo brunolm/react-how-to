7ec3c419eb0983149a38e2139e7f34992a0a91fb
Bruno Leonardo Michels <brunolm7@gmail.com>
2017-11-19 22:39:22 -0200

    Add a test with enzyme shallow

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,8 +1,10 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import { shallow } from './specHelper';
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const component = shallow(App, {});
+    expect(component).toBeTruthy();
+  });
 });
